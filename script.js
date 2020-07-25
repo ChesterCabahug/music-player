508c6e24c753f74c4bebd8b75bdc2f7b119e8bb8
Chester <chesterdeveloper2000@gmail.com>
2020-07-25 13:07:58 +0800

    app now displays the duration of the song correctly!

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const artist = document.getElementById("artist")
 const music = document.querySelector("audio")
 const progressContainer = document.getElementById("progress-container")
 const progress = document.getElementById("progress")
+const currentTimeEl = document.getElementById("current-time")
+const durationEl = document.getElementById("duration")
 const prevBtn = document.getElementById("prev")
 const playBtn = document.getElementById("play")
 const nextBtn = document.getElementById("next")
@@ -97,11 +99,27 @@ loadSong(songs[songIndex])
 updateProgressBar = (e) => {
     if(isPlaying) {
         const {duration, currentTime} = e.srcElement
-        console.log(duration, currentTime)
         // update progress bar width
         const progressPercent = (currentTime / duration) * 100
         progress.style.width = `${progressPercent}%`
+
+        // calculate display for duration
+        const durationMinutes = Math.floor(duration / 60) 
+
+        console.log("minutes!", durationMinutes)
+        let durationSeconds = Math.floor(duration % 60)
+        if(durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`
+        }
+        console.log("seconds!!",durationSeconds)
+
+        
+        // delay switching duration element to avoid NaN
+        if(durationSeconds) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`
+        }
     }
+
 }
 
 // event listeners 
