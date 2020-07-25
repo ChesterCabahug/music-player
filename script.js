dd138d45ffc0dd1b82114bbda6537d6455f11111
Chester <chesterdeveloper2000@gmail.com>
2020-07-25 15:19:49 +0800

    when you finish the track, the next music will finally play!

--- script.js
+++ script.js
@@ -135,21 +135,20 @@ updateProgressBar = (e) => {
 // setProgressBar = (e) =>
 function setProgressBar (e) {
     const width = this.clientWidth
-    console.log("width:", width)
     const clickX = e.offsetX
-    console.log("click X", clickX)
 
     const { duration } = music
 
-    console.log(clickX / width)
-    console.log((clickX / width) * duration)
 
 
     music.currentTime = (clickX / width) * duration
+
+
 }
 
 // event listeners 
 prevBtn.addEventListener("click", prevSong)
 nextBtn.addEventListener("click", nextSong)
+music.addEventListener("ended", nextSong)
 music.addEventListener("timeupdate", updateProgressBar)
 progressContainer.addEventListener("click", setProgressBar)
